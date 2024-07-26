30a132c4e24cd439081eb73e61fc535d6f7ad986
sazedur-strativ <sazedur.rahman@strativ.se>
2024-07-26 16:02:03 +0600

    exercise 2 completed

---
+++ src/LogInForm.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/prop-types */
+import styled from "styled-components";
+
+export const LogInForm = ({handleSubmit}) => (
+        <Wrapper  onSubmit={handleSubmit}>
+      <Label>
+        Email:
+        <TextInput
+          type="email"
+          placeholder="me@you.com"
+        />
+      </Label>
+      <Label>
+        Password:
+        <TextInput
+          type="password"
+        />
+      </Label>
+      <SubmitButton type="submit">Log In</SubmitButton>
+    </Wrapper>
+    );
+
+ const Wrapper =styled.form`
+  width: max-content;
+  margin: 16px auto;
+  border: 1px dotted silver;
+  padding: 26px 32px 32px;
+  border-radius: 2px;
+
+ `
+ const Label = styled.label`
+ display: block;
+  margin-bottom: 16px;
+ `
+const TextInput = styled.input`
+display: block;
+  width: 175px;
+  border: 1px solid black;
+  border-bottom-width: 2px;
+  padding: 6px 8px;
+  margin-top: 4px;
+  border-radius: 2px 2px 3px 3px;
+
+  &:focus{
+  outline: 3px auto blue;
+  outline-offset: 2px;
+  border-color: transparent;
+  }
+`
+const SubmitButton = styled.button`
+display: block;
+  margin-top: 40px;
+  width: 100%;
+  background: black;
+  color: white;
+  padding: 8px;
+  border: none;
+  font-size: 1rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 3px;
+
+  &:focus{
+  outline: 3px auto blue;
+  background: blue;
+  outline-offset: 2px;
+  }
+
+`
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import FrequentlyAskedQuestion from './FrequentlyAskedQuestion.jsx'
 import './index.css'
+import { LogInForm } from './LogInForm.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-<FrequentlyAskedQuestion/>
+    <FrequentlyAskedQuestion
+    question="What does “CSS” stand for?"
+    answer="Cool Styling Strategy"
+  />
+  <LogInForm
+  handleSubmit={(ev)=>{
+    ev.preventDefault();
+    alert('Submitted')
+  }}
+  />
   </React.StrictMode>,
 )
